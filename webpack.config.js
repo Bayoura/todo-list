7d214decf956c7f25533fd99c12817495766853b
Bayoura <Kristin.Seidelmann@gmx.de>
2022-03-03 15:49:23 +0100

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            }, 
+        ]   
+    },  
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'To Do List',
+            filename: 'index.html',
+            template: path.resolve(__dirname, 'src/temp.html')
+        }) 
+    ],
+}
