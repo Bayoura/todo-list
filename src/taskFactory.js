cf6bc3cc7ca89df13921093e2609eafdd7d34de6
Bayoura <Kristin.Seidelmann@gmx.de>
2022-03-09 15:32:05 +0100

    rename

--- src/taskFactory.js
+++
@@ -1,9 +0,0 @@
-export default function taskFactory(title, description, date, priority) {
-    const task = {};
-    task.title = title;
-    task.description = description;
-    task.date = date;
-    task.priority = priority;
-    console.log(task);
-    return task;
-}
