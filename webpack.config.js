3036d95e21b9b478e39651f9dd09ee2a56db7b0c
Bayoura <Kristin.Seidelmann@gmx.de>
2022-03-29 19:50:00 +0200

    add svg loader

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,10 @@ module.exports = {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
             }, 
+            {
+                test: /\.(svg|eot|woff|woff2|ttf)$/,
+                use: ['file-loader'],
+            },
         ]   
     },  
     plugins: [
