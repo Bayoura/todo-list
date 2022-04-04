3eeba2cb81e2258f24367d76258c035b3a26a948
Bayoura <Kristin.Seidelmann@gmx.de>
2022-04-04 14:26:29 +0200

    add local storage

--- src/factories.js
+++ src/factories.js
@@ -1,4 +1,6 @@
 const factories = (() => {
+    let userProjectList = [];
+    
     function taskFactory(title, description, date, priority) {
         const task = {};
         task.title = title;
@@ -6,7 +8,6 @@ const factories = (() => {
         task.date = date;
         task.priority = priority;
         task.done = false;
-        console.log(task);
         return task;
     }
     
@@ -17,9 +18,30 @@ const factories = (() => {
         return project;
     }
 
+    if(!localStorage.getItem('projectList')) {  
+        let demoProject =  projectFactory('Demo Project');
+        demoProject.tasks.push(
+            taskFactory('Homework','English book, p.25, exercisies 2 and 3','12.01.2023','high'), 
+            taskFactory('Go grocery shopping','buy apples','12.01.2023','high')
+            );
+        userProjectList.push(demoProject);
+        // console.log(userProjectList);
+        // console.log(demoProject);
+    } else {    
+        const storagedProjects = JSON.parse(localStorage.getItem('projectList')); 
+        userProjectList = storagedProjects;
+    } 
+
+// function saveLocal() {
+//     localStorage.setItem('library', JSON.stringify(myLibrary)); 
+//     let books = JSON.parse(localStorage.getItem('library')); 
+//     return books;
+// }
+
     return {
         taskFactory,
-        projectFactory
+        projectFactory,
+        userProjectList
     };
 })();
 
