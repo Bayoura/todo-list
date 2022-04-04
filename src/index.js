c79c0f7ef0763093c0601c1e3ec3bf21a8d27282
Bayoura <Kristin.Seidelmann@gmx.de>
2022-04-04 14:26:53 +0200

    add renderProjects and renderTasks functions

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import '@fortawesome/fontawesome-free/css/regular.css';
 import '@fortawesome/fontawesome-free/css/solid.css';
 import '@fortawesome/fontawesome-free/css/fontawesome.min.css';
 import addHandlers from './handlers.js';
+import dom from './dom.js';
+import factories from './factories.js';
 
 
 // make it so that users can switch a todo from one project to another
@@ -12,4 +14,6 @@ import addHandlers from './handlers.js';
 // when overlay is active you can still focus on list items etc. underneath the overlay (with tab)
 // if project or task title input is longer than x make it like so "this is a long..." and only display the full title when you click on the details
 
-addHandlers.addNavEvents()
+addHandlers.addNavEvents();
+dom.renderProjects();
+dom.renderTasks(factories.userProjectList[0]);
