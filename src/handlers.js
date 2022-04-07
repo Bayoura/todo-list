089668dbc99c2fdbb183ce80a9a347383481ad72
Bayoura <Kristin.Seidelmann@gmx.de>
2022-04-07 16:46:01 +0200

    sort handlers into functions

--- src/handlers.js
+++ src/handlers.js
@@ -5,9 +5,26 @@ const addHandlers = (() => {
 
     function addNavEvents() {
         const sidebarTabs = document.querySelectorAll('[data-sidebarTab]');
+        const addProject_button = document.querySelector('[data-addProjectBtn]');
+        const cancelProject_button = document.querySelector('[data-cancelProjectBtn]');
+
         sidebarTabs.forEach(tab => tab.addEventListener('click', e => {
             chooseProject(e)
         }));
+        addProject_button.addEventListener('click', dom.displayProjectForm)
+        cancelProject_button.addEventListener('click',  dom.displayProjectForm) 
+        submitProject();       
+    }
+    
+    function addMainEvents() {
+        submitTask();
+        addSortHandler();
+        const cancelTask_button = document.querySelector('[data-cancelTaskBtn]');
+        const overlay_div = document.querySelector('[data-overlay]');
+        const hamburger_label = document.querySelector('[data-hamburger]');
+        cancelTask_button.addEventListener('click', dom.displayModal);
+        overlay_div.addEventListener('click', dom.displayModal);
+        hamburger_label.addEventListener('click', dom.toggleSidebar);
     }
 
     function chooseProject(e) {
@@ -15,9 +32,16 @@ const addHandlers = (() => {
             return;
         } else {
             setCurrentClass(e.target);
-            let currentProject = factories.projectList[determineCurrentProjectId()];
-            dom.renderTasks(currentProject, determineCurrentProjectId());
-            dom.renderHeader(currentProject, determineCurrentProjectId());
+            let id = determineCurrentProjectId();
+            let currentProject = factories.projectList[id];
+            // check if it is a default or user project
+            if (id < 6) {
+                dom.determineTasks(currentProject, id);
+                dom.renderHeader(currentProject, id);
+            } else {
+                dom.renderTasks(currentProject);
+                dom.renderHeader(currentProject, id);
+            }
         }
     }
 
@@ -50,105 +74,72 @@ const addHandlers = (() => {
         }
         return allTasks;
     }
-
-    const submitTask_button = document.querySelector('[data-submitTaskBtn]');
-    const taskForm_form = document.querySelector('[data-taskModalForm]');
-    const addTask_button = document.querySelector('[data-addTaskBtn]');
-    const modal_div = document.querySelector('[data-modal]');
-    const overlay_div = document.querySelector('[data-overlay]');
-
-    addTask_button.addEventListener('click', () => {
-        modal_div.classList.remove('closed');
-        overlay_div.classList.remove('closed');
-    });
-
-    // tasks
-    document.addEventListener('DOMContentLoaded', () => {    
-        submitTask_button.addEventListener('click', e => {    
-            e.preventDefault(); //stop form from submitting   
-
-            //check if required fields are filled out
-            let checkStatus = taskForm_form.checkValidity();
-            taskForm_form.reportValidity();
-            if (checkStatus) {
-                const title = document.querySelector('[data-taskTitleInput]').value;
-                const description = document.querySelector('[data-descriptionInput]').value;
-                let date = document.querySelector('[data-dateInput]').value;
-                date = new Date();
-                const priorityValue = document.querySelector('input[name=priority]:checked').value;
-                const newTask = factories.taskFactory(title, description, date, priorityValue);
-                
-                factories.projectList[determineCurrentProjectId()].tasks.push(newTask);
-                dom.renderTasks(factories.projectList[determineCurrentProjectId()]);
-                dom.renderHeader(factories.projectList[determineCurrentProjectId()], determineCurrentProjectId());
-            }
-        })    
-    });
-
-    const addProject_button = document.querySelector('[data-addProjectBtn]');
-    const newProject_form = document.querySelector('[data-newProjectForm]');
-    const submitProject_button = document.querySelector('[data-submitProjectBtn]');
-    const cancelProject_button = document.querySelector('[data-cancelProjectBtn]');
-
-    addProject_button.addEventListener('click', () => newProject_form.classList.toggle('closed'));
-    cancelProject_button.addEventListener('click', () => newProject_form.classList.add('closed'));
-
     
-    // projects
-    document.addEventListener('DOMContentLoaded', () => {    
-        submitProject_button.addEventListener('click', e => {    
-            e.preventDefault(); //stop form from submitting   
+    function submitTask() {
+        const submitTask_button = document.querySelector('[data-submitTaskBtn]');
+        const taskForm_form = document.querySelector('[data-taskModalForm]');
+        document.addEventListener('DOMContentLoaded', () => {    
+            submitTask_button.addEventListener('click', e => {    
+                e.preventDefault(); //stop form from submitting   
+    
+                //check if required fields are filled out
+                let checkStatus = taskForm_form.checkValidity();
+                taskForm_form.reportValidity();
+                if (checkStatus) {
+                    const title = document.querySelector('[data-taskTitleInput]').value;
+                    const description = document.querySelector('[data-descriptionInput]').value;
+                    let date = document.querySelector('[data-dateInput]').value;
+                    date = new Date(date);
+                    const priorityValue = document.querySelector('input[name=priority]:checked').value;
+                    const newTask = factories.taskFactory(title, description, date, priorityValue);
+                    
+                    factories.projectList[determineCurrentProjectId()].tasks.push(newTask);
+                    dom.renderTasks(factories.projectList[determineCurrentProjectId()]);
+                    dom.renderHeader(factories.projectList[determineCurrentProjectId()], determineCurrentProjectId());
+                    dom.displayModal();
+                }
+            })    
+        });
+    }
+    
+    function submitProject() {
+        const submitProject_button = document.querySelector('[data-submitProjectBtn]');
+        const newProject_form = document.querySelector('[data-newProjectForm]');
+        document.addEventListener('DOMContentLoaded', () => {    
+            submitProject_button.addEventListener('click', e => {    
+                e.preventDefault(); //stop form from submitting   
+    
+                //check if required fields are filled out
+                let checkStatus = newProject_form.checkValidity();
+                newProject_form.reportValidity();
+                if (checkStatus) {
+                    const title = document.querySelector('[data-projectTitleInput]').value;
+                    const newProject = factories.projectFactory(title);
+                    factories.projectList.push(newProject);
+                    dom.renderProjects();
+                    dom.displayProjectForm();
+                }
+            })    
+        });
+    }
 
-            //check if required fields are filled out
-            let checkStatus = newProject_form.checkValidity();
-            newProject_form.reportValidity();
-            if (checkStatus) {
-                const title = document.querySelector('[data-projectTitleInput]').value;
-                const newProject = factories.projectFactory(title);
-                factories.projectList.push(newProject);
-                dom.renderProjects();
-            }
-        })    
-    });
-
-    const cancelTask_button = document.querySelector('[data-cancelTaskBtn]');
-
-    cancelTask_button.addEventListener('click', () => {
-        modal_div.classList.add('closed');
-        overlay_div.classList.add('closed');
-    })
-
-    const sortButton_div = document.querySelector('[data-sortButton]');
-    const sortOptions_div = document.querySelector('[data-sortOptions]');
-    const sortDate_div = document.querySelector('[data-sortDate]');
-    const sortImportance_div = document.querySelector('[data-sortImportance]');
-    const sortCustom_div = document.querySelector('[data-sortCustom]');
-    const sortOverlay_div = document.querySelector('[data-sortOverlay]');
-
-    sortButton_div.addEventListener('click', () => {
-        sortOptions_div.classList.add('show');
-        sortOverlay_div.classList.remove('closed');
-    })
-
-    sortOverlay_div.addEventListener('click', () => {
-        console.log('clicked');
-        sortOptions_div.classList.remove('show');
-        sortOverlay_div.classList.add('closed');
-    });
-
-    const hamburger_label = document.querySelector('[data-hamburger]');
-    const sidebar_nav = document.querySelector('[data-sidebar]');
-    const main_element = document.querySelector('[data-main]')
-
-    hamburger_label.addEventListener('click', () => {
-        sidebar_nav.classList.toggle('hidden');
-        main_element.classList.toggle('full-page');
-    })
+    function addSortHandler() {
+        const sortButton_div = document.querySelector('[data-sortButton]');
+        const sortNewOld_div = document.querySelector('[data-sortNewOld]');
+        const sortOldNew_div = document.querySelector('[data-sortOldNew]');
+        const sortDate_div = document.querySelector('[data-sortDate]');
+        const sortImportance_div = document.querySelector('[data-sortImportance]');
+        const sortOverlay_div = document.querySelector('[data-sortOverlay]');
+    
+        sortButton_div.addEventListener('click', dom.displaySortOptions);
+        sortOverlay_div.addEventListener('click', dom.displaySortOptions);
+    }
 
     return {
         addNavEvents,
+        addMainEvents,
         chooseProject,
-        getAllTasks
+        getAllTasks,
     }
 
 })();
