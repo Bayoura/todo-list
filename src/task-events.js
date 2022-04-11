131465157ecb5dcb74a347b19945c351219913c9
Bayoura <Kristin.Seidelmann@gmx.de>
2022-04-11 17:53:12 +0200

    add editTask und submitEdit functions

--- src/task-events.js
+++ src/task-events.js
@@ -114,36 +114,46 @@ const taskEvents = (() => {
                 dom.renderHeader(currentProject, currentProjectId)
             }, 200); 
         }
-        console.log(clickedTask);
-        console.log(factories.projectList);
-    }
-
-    function showTaskDetails() {
-
     }
     
     function editTask(clicked) {
-        dom.displayModal();
-        const currentProject = factories.projectList[addHandlers.determineCurrentProjectId()];
+        dom.toggleModal();
         const currentProjectId = addHandlers.determineCurrentProjectId();
+        const currentProject = factories.projectList[currentProjectId];
         const clickedTask = currentProject.tasks[clicked.dataset.id];
-        const originProject = factories.projectList[clickedTask.projectId];
-        originProject.tasks.splice(clickedTask.taskId, 1);  
 
         document.querySelector('[data-taskTitleInput]').value = clickedTask.title;
         document.querySelector('[data-descriptionInput]').value = clickedTask.description;
         document.querySelector('[data-dateInput]').value = dayjs(clickedTask.dueDate).format('YYYY-MM-DD');  
-        // document.querySelector('input[name=priority]:checked').value = clickedTask.priority;
-
-        // if (currentProjectId < 6) {
-        //     determineTasks(currentProject, currentProjectId);
-        // } else {
-        //     dom.renderTasks(currentProject);
-        // }
-        // dom.renderHeader(currentProject, currentProjectId)
-
+        document.querySelectorAll('input[name=priority]').forEach(input => {
+            if (input.value === clickedTask.priority) input.checked = true;
+        })
+        const submitEdit_button = document.querySelector('[data-submitEditBtn]');
+        submitEdit_button.onclick = e => submitEdit(e, clickedTask);
     }
 
+    function submitEdit(e, clickedTask) {
+        e.preventDefault(); //stop form from submitting   
+        
+        //check if required fields are filled out
+        const taskForm_form = document.querySelector('[data-taskModalForm]');
+        let checkStatus = taskForm_form.checkValidity();
+        taskForm_form.reportValidity();
+        if (checkStatus) {
+            const currentProject = factories.projectList[addHandlers.determineCurrentProjectId()];
+            const projectId = addHandlers.determineCurrentProjectId();
+            clickedTask.title = document.querySelector('[data-taskTitleInput]').value;
+            clickedTask.description = document.querySelector('[data-descriptionInput]').value;
+            clickedTask.dueDate = document.querySelector('[data-dateInput]').value;
+            clickedTask.priority = document.querySelector('input[name=priority]:checked').value;
+            
+            dom.renderTasks(currentProject);
+            dom.renderHeader(currentProject, projectId);
+            dom.toggleModal();
+        } 
+    }
+     
+    
     // note: the data-id attribute shows the index of the task in the current list (the default projects, e.g. 'All'),
     // the taskId shows the index of the task in the original task list of the project the tasks belongs to (the user projects)
     function deleteTask(clicked) {
@@ -165,7 +175,6 @@ const taskEvents = (() => {
     return {
         determineTasks,
         changeCompletionStatus,
-        showTaskDetails,
         editTask,
         deleteTask,
     }
