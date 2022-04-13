42a2fa68cc99fdff4e77a1ce222a5a1826136d6f
Bayoura <Kristin.Seidelmann@gmx.de>
2022-04-13 17:54:04 +0200

    add rename functions

--- src/project-events.js
+++ src/project-events.js
@@ -4,54 +4,86 @@ import addHandlers from './handlers.js';
 
 const projectEvents = (() => {
 
-
-    function displayInput() {
-        // const inp = document.createElement('input');
-        // inp.type = 'text';
-
-        // const listItems = document.querySelectorAll('[data-sidebarTab]');
-        // listItems[6];
-
-        const currentProjectId = addHandlers.determineCurrentProjectId();
-        const currentProject = factories.projectList[currentProjectId];
-        console.log(currentProject)
-    }
-
     function renameProject(clicked) {
-        displayInput();
-        // const currentProjectId = addHandlers.determineCurrentProjectId();
-        // const currentProject = factories.projectList[currentProjectId];
-        // const clickedTask = currentProject.tasks[clicked.dataset.id];
+        // this ensures that user can only rename one project at a time
+        if (document.querySelectorAll('[data-submitRenaming]').length > 0) {
+            dom.renderProjects(addHandlers.determineCurrentProjectId());
+        } 
+        // when projects are newly rendered, the variable `clicked` points to an element that doesn't exist anymore
+        // because after the click, the element was erased (when above condition is fulfilled) and an identical copy was rendered
+        // `newElement` is the same as `clicked` but it points to the current rendered element
+        const renameButtons = document.querySelectorAll('[data-projectRename]');
+        let newElement = null; 
+        renameButtons.forEach(button => {
+            if (button.dataset.id === clicked.dataset.id) newElement = button;
+        })
 
-        // document.querySelector('[data-taskTitleInput]').value = clickedTask.title;
-        // document.querySelector('[data-descriptionInput]').value = clickedTask.description;
-        // document.querySelector('[data-dateInput]').value = dayjs(clickedTask.dueDate).format('YYYY-MM-DD');  
-        // document.querySelectorAll('input[name=priority]').forEach(input => {
-        //     if (input.value === clickedTask.priority) input.checked = true;
-        // })
-        // const submitEdit_button = document.querySelector('[data-submitEditBtn]');
-        // submitEdit_button.onclick = e => submitEdit(e, clickedTask);
+        const iconDiv = newElement.parentElement;
+        const span = document.createElement('span');
+        // submit button
+        const submitRenamingBtn = document.createElement('submit');
+        submitRenamingBtn.setAttribute('data-submitRenaming', '');
+        submitRenamingBtn.setAttribute('data-id', `${newElement.dataset.id}`);
+        submitRenamingBtn.setAttribute('form', 'rename-project');
+        submitRenamingBtn.ariaLabel = 'save edit';
+        submitRenamingBtn.classList.add(
+            'fa-regular',
+            'fa-circle-check',
+            'icon'
+            ); 
+            
+            
+        span.append(submitRenamingBtn)
+        iconDiv.insertBefore(span, iconDiv.firstChild);
+        const clickedProject = factories.projectList[newElement.dataset.id];
+        
+        // input field
+        const renameForm = document.createElement('form');
+        const renameInput = document.createElement('input');
+        renameForm.action = '';
+        renameForm.id = 'rename-project'
+        renameForm.setAttribute('data-renameForm', '');
+        renameInput.setAttribute('data-renameInput', '');
+        renameInput.type = 'text';
+        renameInput.required = true;
+        renameInput.classList.add('rename-input');
+        renameInput.value = clickedProject.title;
 
+        renameForm.append(renameInput);
+
+        document.querySelectorAll('[data-sidebarTab]').forEach(tab => {
+            if (tab.dataset.id === newElement.dataset.id) {
+                tab.textContent = '';
+                tab.append(renameForm);
+            }
+        })
+        newElement.remove();
+        
+
+        // if (document.querySelectorAll('[data-submitRenaming]').length === 1) {
+        //     console.log(document.querySelectorAll('[data-submitRenaming]'))
+        //     const body = document.querySelector('body');
+        //     body.addEventListener('click', e => {
+        //         if (!e.target.classList.contains('rename-input') && !e.target.hasAttribute('data-submitRenaming')) {
+        //             dom.renderProjects(addHandlers.determineCurrentProjectId());
+        //         }
+        //     })
+        // }
     }
 
-    function submitRename(e, clickedTask) {
+    function submitRenaming(e, clicked) {
         e.preventDefault(); //stop form from submitting   
         
         //check if required fields are filled out
-        const taskForm_form = document.querySelector('[data-taskModalForm]');
-        let checkStatus = taskForm_form.checkValidity();
-        taskForm_form.reportValidity();
+        const renameForm_form = document.querySelector('[data-renameForm]');
+        let checkStatus = renameForm_form.checkValidity();
+        renameForm_form.reportValidity();
         if (checkStatus) {
-            const currentProject = factories.projectList[addHandlers.determineCurrentProjectId()];
-            const projectId = addHandlers.determineCurrentProjectId();
-            clickedTask.title = document.querySelector('[data-taskTitleInput]').value;
-            clickedTask.description = document.querySelector('[data-descriptionInput]').value;
-            clickedTask.dueDate = document.querySelector('[data-dateInput]').value;
-            clickedTask.priority = document.querySelector('input[name=priority]:checked').value;
-            
-            dom.renderTasks(currentProject);
-            dom.renderHeader(currentProject, projectId);
-            dom.toggleModal();
+            const clickedProject = factories.projectList[clicked.dataset.id];
+            clickedProject.title = document.querySelector('[data-renameInput]').value;
+
+            dom.renderProjects(addHandlers.determineCurrentProjectId());
+            dom.renderHeader(factories.projectList[addHandlers.determineCurrentProjectId()], addHandlers.determineCurrentProjectId());
         } 
     }
 
@@ -72,6 +104,7 @@ const projectEvents = (() => {
 
     return {
         renameProject,
+        submitRenaming,
         deleteProject
     }
 })()
